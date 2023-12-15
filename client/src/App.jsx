471e94f32ff302544887c7974a0908b74532c47a
Zyrridian <rezkyaf246@gmail.com>
2023-12-15 18:11:37 +0700

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 import Header from './components/Header'
 
 export default function App() {
@@ -15,8 +15,8 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
-        <Route path='/signout' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
