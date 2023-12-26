c59c248dc7e61891a340749cd8c99e5b8f045767
Zyrridian <rezkyaf246@gmail.com>
2023-12-26 13:25:46 +0700

    create sign in api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to='/sign-in'>
+                <Link to='/signin'>
                     <li className='text-slate-700 hover:underline'>Sign In</li>
                 </Link>
             </ul>
