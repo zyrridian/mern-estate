ee9c93f5fcc52d4ff15ce12634ed4c5b796ac0c6
zyrridian <rezkyaf246@gmail.com>
2024-01-04 00:06:10 +0700

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi saepe quaerat voluptatem, magnam modi suscipit recusandae esse autem molestiae voluptas officia maxime aperiam. Nihil at doloremque corporis possimus temporibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis veniam minus, natus ipsa nobis aspernatur in. Repellat ex officia accusamus ratione! At, commodi suscipit? Quae blanditiis quaerat iste illum commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores earum quibusdam explicabo ab hic inventore ea expedita nobis voluptatum saepe sunt incidunt nulla, recusandae veniam eius eveniet esse in laborum.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi saepe quaerat voluptatem, magnam modi suscipit recusandae esse autem molestiae voluptas officia maxime aperiam. Nihil at doloremque corporis possimus temporibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis veniam minus, natus ipsa nobis aspernatur in. Repellat ex officia accusamus ratione! At, commodi suscipit? Quae blanditiis quaerat iste illum commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores earum quibusdam explicabo ab hic inventore ea expedita nobis voluptatum saepe sunt incidunt nulla, recusandae veniam eius eveniet esse in laborum.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi saepe quaerat voluptatem, magnam modi suscipit recusandae esse autem molestiae voluptas officia maxime aperiam. Nihil at doloremque corporis possimus temporibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis veniam minus, natus ipsa nobis aspernatur in. Repellat ex officia accusamus ratione! At, commodi suscipit? Quae blanditiis quaerat iste illum commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores earum quibusdam explicabo ab hic inventore ea expedita nobis voluptatum saepe sunt incidunt nulla, recusandae veniam eius eveniet esse in laborum.</p>
+    </div>
   )
 }
